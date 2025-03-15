81e626fdf73316d8558d7d1184386dfc7bc01310
Joseph <egiemheyo@gmail.com>
2025-03-15 15:38:25 +0100

    update added

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,25 @@ const nextConfig = {
   reactStrictMode: false,
   // reactStrictMode: true,
   serverRuntimeConfig: {
-    dbConfig: {
-      host: "localhost",
-      port: 3306,
-      user: "root",
-      password: "Alenoghena@2",
-      database: "nextdb",
+    development: {
+      dbConfig: {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Alenoghena@2",
+        database: "nextdb",
+      },
+      production: {
+        dbConfig: {
+          host: process.env.DB_PROD_HOST,
+          port: process.env.DB_PORT,
+          user: process.env.DB_PROD_USERNAME,
+          password: process.env.DB_PROD_PASSWORD,
+          database: process.env.DB_PROD_NAME,
+        },
+      },
     },
+
     secret:
       "bd1c82768d21e48586ad4264b39d727c5509f26c12fb482264c53b1d137974e8c736a5223479c7c39551e6466e85d3fc9e6e17421",
     // secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
@@ -19,7 +31,7 @@ const nextConfig = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/api" // development api
-        : "http://localhost:3000/api", // production api
+        : `${process.env.DB_PROD_HOST}/api`, // production api
   },
   experimental: {
     serverActions: {
